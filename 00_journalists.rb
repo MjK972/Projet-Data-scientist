2fad8468ad18d44dd8fd1a224945696d92e1c31a
MjK972 <59626423+MjK972@users.noreply.github.com>
2020-01-16 21:49:33 +0100

    Create 00_journalists.rb

---
+++ 00_journalists.rb
@@ -0,0 +1,177 @@
+journaliste = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag",
+"@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy",
+"@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson",
+"@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama",
+"@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier",
+"@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre",
+"@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa",
+"@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab",
+"@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader",
+"@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet",
+"@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy",
+"@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille",
+"@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi",
+"@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin",
+"@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur",
+"@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot",
+"@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari",
+"@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal",
+"@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout",
+"@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas",
+"@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval",
+"@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli",
+"@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani",
+"@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1",
+"@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther",
+"@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet",
+"@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc",
+"@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD",
+"@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF",
+"@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez",
+"@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie",
+"@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen",
+"@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine",
+"@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean",
+"@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz",
+"@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache",
+"@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx",
+"@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES",
+"@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard",
+"@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz",
+"@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari",
+"@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul",
+"@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari",
+"@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar",
+"@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier",
+"@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM",
+"@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet",
+"@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer",
+"@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio",
+"@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud",
+"@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben",
+"@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick",
+"@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin",
+"@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc",
+"@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser",
+"@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+#Question 1
+puts " combien y-a t'il de journaliste dans cet array ?"
+puts " il y a #{journaliste.length} journaliste dans mon array"
+
+
+#Question 2
+puts " combien d'handles contiennent un numero ?"
+count = 0							  #def variable incrémentation
+journaliste.each do |email|           #each do = pour chaque element de mon array
+	if email.match(/[0-9]/)           #match= matcher [correspond] 
+		count +=1
+	end
+end
+puts "#{count}"
+
+
+#Question 3
+puts "Combien d'handle contiennent les 4 lettres du prénom 'Aude' à la suite (sans faire attention à la casse) ? "
+
+puts " combien d'handles contiennent un numero ?"
+count = 0							              #def variable incrémentation
+journaliste.each do |email|                       #each do = pour chaque element de mon array
+	if email.match(/[Aa][Uu][Dd][Ee]/)            #match= matcher [correspond] 
+		count +=1
+	end
+end
+puts "#{count}"
+
+
+#Question 4
+
+puts "Combien commencent par une majuscule ?"
+
+count = 0							              
+journaliste.each do |email|               
+	if email.match(/@[A-Z]/)            
+		count +=1
+	end
+end
+puts "#{count}"
+
+
+#Question 5
+
+puts "Combien contiennent une majuscule ?"
+
+count = 0							              
+journaliste.each do |email|               
+	if email.match(/[A-Z]/)            
+		count +=1
+	end
+end
+puts "#{count}"
+
+
+#Question 6
+
+puts "Combien de _ dans tous les pseudos confondus ?"
+
+count = 0							              
+journaliste.each do |email|               
+	if email.match(/_/)            
+		count +=1
+	end
+end
+puts "#{count}"
+
+
+#Question 7
+
+puts "Trie la liste de handle par ordre alphabétique "
+
+journaliste_sans = []                                        # Créer tableau vide pour y mettre les nouveaux mail sans les @ et les _
+count = 0
+journaliste.each do |mail|                                   # Pour chaque mail du tableau 'journalistes'
+	journaliste_sans.push(mail.gsub(/[@_]/, "").capitalize)  # .gsub [@_], "" permet de remplacer les @ et les _ par une chaine/caractère vide
+end                                                          # .capitalize permet de mettre une majuscule sur la première lettre de chaque email pour pouvoir les trier dans l'ordre ensuite 
+                                                             # Si on ne met pas .capitalize, cela va d'abord trié les majuscules de 'A' à 'Z' puis les minuscules de 'a' à 'z' puis le reste ...  
+puts "#{journaliste_sans.sort}"                              # .sort permet de trier dans l'ordre le tableau journaliste_sans
+
+
+	#.sort= trier dans l'ordre num ou alphabetique
+
+
+
+#Question 8
+
+puts "Quels sont les 50 handles les plus courts de ce array ?"
+
+
+
+
+#Question 9
+
+puts "Quelle est la position dans l'array de la personne @epenser ?"
+
+puts journaliste.index('@epenser')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
